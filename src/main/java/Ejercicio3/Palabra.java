687b8ffc81dbe58e709510ec6e1de7d1d66e924c
rafael2347 <gonzalezmartinrafael1@gmail.com>
2024-04-04 01:46:27 +0200

    Realizado ejercicio 3 con todos sus métodos

--- src/main/java/Ejercicio3/Palabra.java
+++ src/main/java/Ejercicio3/Palabra.java
@@ -1,7 +1,32 @@
 package Ejercicio3;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.util.StringTokenizer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Palabra {
     private String palabra;
@@ -24,27 +49,103 @@ public class Palabra {
                 "palabra='" + palabra + '\'' +
                 '}';
     }
+
     public String devolverMayusculas() {
         return palabra.toUpperCase();
     }
-    public String devolverMinusculas(){
+
+    public String devolverMinusculas() {
         return palabra.toLowerCase();
     }
-    public int ContadorPalabras() {
-        if (palabra == null || palabra.isEmpty()) {
-            return 0; // Si la cadena está vacía, no hay palabras
-        }
 
-        int contador = 0;
+    public int ContadorPalabras() {
+        return palabra.length();
+    }
+
+    public String reemplazarLetras(char letraOriginal, char letraReemplazo) {
+        StringBuilder nuevaPalabra = new StringBuilder();
+
         for (int i = 0; i < palabra.length(); i++) {
-            char c = palabra.charAt(i);
-            if (Character.isLetter(c)) {
-                contador++;
+            char caracterActual = palabra.charAt(i);
+            if (caracterActual == letraOriginal) {
+                nuevaPalabra.append(letraReemplazo);
+            } else {
+                nuevaPalabra.append(caracterActual);
             }
         }
 
-        return contador;
+        return nuevaPalabra.toString();
     }
 
+    public String devolverPrimeraLetra() {
+        char letra = palabra.charAt(0);
+        return String.valueOf(letra);
+    }
 
+    public String UltimaLetraMayus() {
+        char letrra = palabra.charAt(palabra.length() - 1);
+        String caddena = Character.toString(letrra);
+        return caddena.toUpperCase();
+    }
+
+    public static void writeXMLPalabras(List<Palabra> listaPalabras, String fileName) throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.newDocument();
+
+        Element palabrasElement = doc.createElement("Palabras");
+        doc.appendChild(palabrasElement);
+
+        for (Palabra palabra : listaPalabras) {
+            Element palabraElement = doc.createElement("Palabra");
+            palabraElement.appendChild(doc.createTextNode(palabra.getPalabra()));
+            palabrasElement.appendChild(palabraElement);
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(fileName));
+        transformer.transform(source, result);
+    }
+
+    public static List<Palabra> readXMLPalabras(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        List<Palabra> palabras = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+        doc.getDocumentElement().normalize();
+
+        NodeList nList = doc.getElementsByTagName("Palabra");
+        for (int i = 0; i < nList.getLength(); i++) {
+            Element element = (Element) nList.item(i);
+            String palabra = element.getTextContent();
+            palabras.add(new Palabra(palabra));
+        }
+        return palabras;
+    }
+
+    public static void writeJSONPalabras(List<Palabra> listaPalabras, String fileName) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonArray jsonPalabras = new JsonArray();
+
+        for (Palabra palabra : listaPalabras) {
+            JsonObject jsonPalabra = new JsonObject();
+            jsonPalabra.addProperty("palabra", palabra.getPalabra());
+            jsonPalabras.add(jsonPalabra);
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName))) {
+            writer.write(gson.toJson(jsonPalabras));
+        }
+    }
+
+    public static List<Palabra> readJSONPalabras(String fileName) throws IOException {
+        Gson gson = new Gson();
+        String jsonContent = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
+        TypeToken<List<Palabra>> token = new TypeToken<List<Palabra>>() {
+        };
+        return gson.fromJson(jsonContent, token.getType());
+    }
 }
--- src/main/java/Ejercicio3/TestPalabra.java
+++ src/main/java/Ejercicio3/TestPalabra.java
@@ -1,40 +1,104 @@
 package Ejercicio3;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestPalabra {
     public static void main(String[] args) {
-        Palabra palabra = new Palabra("FANTASTICO");
-        Palabra palabra2 = new Palabra("telefono");
+        try (FileWriter salida = new FileWriter("ficheros/FicheroCoche.txt")) {
+            Palabra palabra = new Palabra("FANTASTICO");
+            Palabra palabra2 = new Palabra("telefono");
 
-        //Pasar letras para convertirlas a mayúsculas
-        System.out.println("------Mayusculas-----");
-        Mayusculas(palabra, palabra2);
-        System.out.println("------Minusculas-----");
-        Minusculas(palabra, palabra2);
-        System.out.println("------Contar numeros---");
-        Contador(palabra, palabra2);
+            List<Palabra> listaPalabras = new ArrayList<>();
+            listaPalabras.add(palabra);
+            listaPalabras.add(palabra2);
 
+            // Pasar letras para convertirlas a mayúsculas
+            System.out.println("------Mayusculas-----");
+            Mayusculas(palabra, palabra2);
+            System.out.println("------Minusculas-----");
+            Minusculas(palabra, palabra2);
+            System.out.println("------Contar numeros---");
+            Contador(palabra, palabra2);
+            System.out.println("------Reemplazar letras---");
+            ReemplazarLetras(palabra, palabra2, 'A', 'u');
+            ReemplazarLetras(palabra, palabra2, 'e', 'a');
+            System.out.println("------Devolver Primera Letra---");
+            PrimeraLetra(palabra, palabra2);
+            System.out.println("------Devolver Ultima Letra Mayuscula---");
+            UltimaLetra(palabra, palabra2);
+
+            // Escribir y leer archivos
+            salida.write(listaPalabras+"\n");
+            Palabra.writeXMLPalabras(listaPalabras, "ficheros/palabras.xml");
+            Palabra.writeJSONPalabras(listaPalabras, "ficheros/palabras.json");
+
+            List<Palabra> palabrasFromXML = Palabra.readXMLPalabras("ficheros/palabras.xml");
+            List<Palabra> palabrasFromJSON = Palabra.readJSONPalabras("ficheros/palabras.json");
+
+            for (Palabra p : palabrasFromXML) {
+                System.out.println("Palabra desde XML: " + p.getPalabra());
+            }
+
+            for (Palabra p : palabrasFromJSON) {
+                System.out.println("Palabra desde JSON: " + p.getPalabra());
+            }
+
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static void Mayusculas(Palabra palabra, Palabra palabra2) {
-        String palabro1 = palabra.devolverMayusculas();
+    private static void Mayusculas(Palabra palabra1, Palabra palabra2) {
+        String palabro1 = palabra1.devolverMayusculas();
         String palabro2 = palabra2.devolverMayusculas();
         System.out.println(palabro1);
         System.out.println(palabro2);
     }
 
-    private static void Minusculas(Palabra palabra, Palabra palabra2){
-        String word = palabra.devolverMinusculas();
+    private static void Minusculas(Palabra palabra1, Palabra palabra2) {
+        String word1 = palabra1.devolverMinusculas();
         String word2 = palabra2.devolverMinusculas();
-        System.out.println(word);
+        System.out.println(word1);
         System.out.println(word2);
-
     }
 
-    private static void Contador(Palabra palabra, Palabra palabra2){
-        int palabraa = palabra.ContadorPalabras();
+    private static void Contador(Palabra palabra1, Palabra palabra2) {
+        int palabraa1 = palabra1.ContadorPalabras();
         int palabraa2 = palabra2.ContadorPalabras();
-        System.out.println(palabraa);
+        System.out.println(palabraa1);
         System.out.println(palabraa2);
+    }
 
+    private static void ReemplazarLetras(Palabra palabra1, Palabra palabra2, char letraOriginal, char letraReemplazo) {
+        String nuevoPalabra1 = palabra1.reemplazarLetras(letraOriginal, letraReemplazo);
+        String nuevoPalabra2 = palabra2.reemplazarLetras(letraOriginal, letraReemplazo);
+
+        System.out.println("Palabra original 1: " + palabra1.getPalabra());
+        System.out.println("Palabra original 2: " + palabra2.getPalabra());
+        System.out.println("Palabra con letras reemplazadas 1: " + nuevoPalabra1);
+        System.out.println("Palabra con letras reemplazadas 2: " + nuevoPalabra2);
+    }
+
+    private static void PrimeraLetra(Palabra palabra, Palabra palabra2) {
+        String primeraLetraA = palabra.devolverPrimeraLetra();
+        String primeraLetraB = palabra2.devolverPrimeraLetra();
+        System.out.println(primeraLetraA);
+        System.out.println(primeraLetraB);
+    }
+
+    private static void UltimaLetra(Palabra palabra, Palabra palabra2) {
+        String UltimaLetraA = palabra.UltimaLetraMayus();
+        String UltimaLetraB = palabra2.UltimaLetraMayus();
+        System.out.println(UltimaLetraA);
+        System.out.println(UltimaLetraB);
     }
 }
