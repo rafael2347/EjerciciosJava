3d634a6d58e6b05ae08c47121f89a747c13ddb9f
rafael2347 <gonzalezmartinrafael1@gmail.com>
2024-04-03 19:26:05 +0200

    Contador de palabras hecho

--- src/main/java/Ejercicio3/Palabra.java
+++ src/main/java/Ejercicio3/Palabra.java
@@ -1,5 +1,8 @@
 package Ejercicio3;
 
+
+import java.util.StringTokenizer;
+
 public class Palabra {
     private String palabra;
 
@@ -24,4 +27,24 @@ public class Palabra {
     public String devolverMayusculas() {
         return palabra.toUpperCase();
     }
+    public String devolverMinusculas(){
+        return palabra.toLowerCase();
+    }
+    public int ContadorPalabras() {
+        if (palabra == null || palabra.isEmpty()) {
+            return 0; // Si la cadena está vacía, no hay palabras
+        }
+
+        int contador = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = palabra.charAt(i);
+            if (Character.isLetter(c)) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+
 }
--- src/main/java/Ejercicio3/TestPalabra.java
+++ src/main/java/Ejercicio3/TestPalabra.java
@@ -2,11 +2,17 @@ package Ejercicio3;
 
 public class TestPalabra {
     public static void main(String[] args) {
-        Palabra palabra = new Palabra("Fantastico");
+        Palabra palabra = new Palabra("FANTASTICO");
         Palabra palabra2 = new Palabra("telefono");
 
         //Pasar letras para convertirlas a mayúsculas
+        System.out.println("------Mayusculas-----");
         Mayusculas(palabra, palabra2);
+        System.out.println("------Minusculas-----");
+        Minusculas(palabra, palabra2);
+        System.out.println("------Contar numeros---");
+        Contador(palabra, palabra2);
+
     }
 
     private static void Mayusculas(Palabra palabra, Palabra palabra2) {
@@ -14,5 +20,21 @@ public class TestPalabra {
         String palabro2 = palabra2.devolverMayusculas();
         System.out.println(palabro1);
         System.out.println(palabro2);
+    }
+
+    private static void Minusculas(Palabra palabra, Palabra palabra2){
+        String word = palabra.devolverMinusculas();
+        String word2 = palabra2.devolverMinusculas();
+        System.out.println(word);
+        System.out.println(word2);
+
+    }
+
+    private static void Contador(Palabra palabra, Palabra palabra2){
+        int palabraa = palabra.ContadorPalabras();
+        int palabraa2 = palabra2.ContadorPalabras();
+        System.out.println(palabraa);
+        System.out.println(palabraa2);
+
     }
 }
